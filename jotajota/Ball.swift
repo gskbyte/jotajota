7f8e4214ca42910be80340f7031f23e4aac5a637
Jose Alcalá Correa <jose.alcala.correa@gmail.com>
2015-04-23 15:54:40 +0200

    Add base classes

---
+++ jotajota/Ball.swift
@@ -0,0 +1,14 @@
+//
+//  Ball.swift
+//  jotajota
+//
+//  Created by Jose Alcalá-Correa on 23/04/15.
+//  Copyright (c) 2015 XING. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class Ball: SKSpriteNode {
+   
+}
---
+++ jotajota/Bar.swift
@@ -0,0 +1,14 @@
+//
+//  Bar.swift
+//  jotajota
+//
+//  Created by Jose Alcalá-Correa on 23/04/15.
+//  Copyright (c) 2015 XING. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class Bar: SKSpriteNode {
+   
+}
